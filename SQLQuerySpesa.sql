03bdd49180469cb85c4fe395e489d31b2039a234
ALE <93939843+Lionetto96@users.noreply.github.com>
2021-12-23 15:32:33 +0100

    Add files via upload

---
+++ SQLQuerySpesa.sql
@@ -0,0 +1,16 @@
+create database GestioneSpesa
+create table Categoria(
+IdCategory int primary key identity(1,1),
+Nome varchar(20))
+
+create table Spesa(
+Id int identity(1,1) primary key,
+Data date,
+Descrizione varchar(50),
+Utente varchar(20),
+Importo decimal(18,2),
+Approvata bit,
+IdCategory int,
+constraint FK_1 foreign key(IdCategory) references Categoria(IdCategory))
+
+insert into Categoria values ('bollette')
